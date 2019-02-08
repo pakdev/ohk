72bf87a8a93c7124d9caaa988077148d58ad03e3
Peter Kurlak <pkurlak@gmail.com>
2019-02-08 00:26:42 -0600

    Accept either a config file or multiple pairs of (hotkey, exe)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+On-Hot-Key.
+
+Usage:
+  ohk <config>
+  ohk [(<keys> <exe>)...]
+  ohk (-h | --help)
+  ohk --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_config: String,
+    arg_keys: Vec<String>,
+    arg_exe: Vec<String>,
+    flag_version: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
